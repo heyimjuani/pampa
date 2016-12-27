2fb2ff906bb8e8e989223db741902bbe158ec897
Juani Ruiz Echazu <juani@aerolab.co>
2016-12-27 00:12:14 -0300

    leaving only dist files on gh-pages

--- src/data/index.js
+++
@@ -1,9 +0,0 @@
-const path = require('path');
-const fs = require('fs');
-
-module.exports = function(file) {
-  return Object.assign(
-    JSON.parse(fs.readFileSync(path.join(__dirname, '_default.json'))),
-    JSON.parse(fs.readFileSync(path.join(__dirname, path.basename(file.path).split('.')[0] + '.json')))
-  )
-}
